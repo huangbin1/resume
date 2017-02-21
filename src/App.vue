6b40363aea88e4ac77637a90ef9b2abd69fbc2dd
彬彬爱吃东西 <245970461@qq.com>
2017-02-21 21:52:27 +0800

    使用vue-cli建立基本的vue工程

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h>黄彬的简历</h>
+    <md-button>Default</md-button>
+    <md-button class="md-primary">Primary</md-button>
+    <md-button class="md-accent">Accent</md-button>
+    <md-button class="md-warn">Warn</md-button>
+    <md-button class="md-primary" disabled>Disabled</md-button>
+    <md-button class="md-dense">Dense</md-button>
+    <router-view>ddddd</router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
