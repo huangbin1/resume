bbb9c4012b6ec7f007cd266ed93ccc0a950b4c8c
彬彬爱吃东西 <245970461@qq.com>
2017-03-09 10:18:57 +0800

    完善项目内容

--- src/components/github.vue
+++ src/components/github.vue
@@ -1,22 +1,55 @@
 <template>
-  <div class="user">
-   
-
+  <div class="project">
+    <timeline class="timeline">
+        <timeline-item>
+            <h4>云间半导体设备网页</h4>
+            <p>2016/8–2016/08</p>
+            <p><strong>项目概要:</strong>该项目是见最近微信小程序比较火，课余时间用微信小应用做了一个二维码应用</p>
+            <p><strong>项目职责:</strong>因为对新鲜的微信小程序特别好奇，有UI设计，js，html的基础，所以独自做了该项目</p>
+            <p><strong>项目业绩:</strong>功能是实现二维码扫描和二维码生成，该项目在github上已经分享</p>
+           
+        </timeline-item>
+        <timeline-item>
+            <h4>基于vue框架的音乐播放器(github)</h4>
+            <p>2016/12–2017/01</p>
+            <p><strong>项目概要:</strong>该项目为本人课余时间写的项目，项目代码保存在github
+          基于vue.js框架和mintui组件，ES6写了个音乐播放器</p>
+            <p><strong>项目职责:</strong>带着对前端的探索精神，独自完成整个项目</p>
+            <p><strong>项目业绩:</strong>对vue，ES6 ，js，有了更深入的学习;本项目在github上有独立的开源分享</p>
+        </timeline-item>
+        <timeline-item>
+            <h4>基于vue框架的个人简历(github)</h4>
+            <p>2017/02–2017/3</p>
+            <p><strong>项目概要:</strong>该项目是基于vue,vux上做的一份个人简历</p>
+            <p><strong>项目职责:</strong>之前用过vue，但用的是mintui组件库，这次换了vux组件库</p>
+            <p><strong>项目业绩:</strong>该项目在github上已经分享</p>
+        </timeline-item>
+        <timeline-item>
+            <h4>基于微信小应用的二维码小助手(github)</h4>
+            <p>2017/03–2017/03</p>
+            <p><strong>项目概要:</strong>该项目是见最近微信小程序比较火，课余时间用微信小应用做了一个二维码应用</p>
+            <p><strong>项目职责:</strong>因为对新鲜的微信小程序特别好奇，有UI设计，js，html的基础，所以独自做了该项目</p>
+            <p><strong>项目业绩:</strong>功能是实现二维码扫描和二维码生成，该项目在github上已经分享</p>
+        </timeline-item>
+        
+    </timeline>
   </div>
 </template>
 
 <script>
-    
-   
-    
+    import {
+        Timeline,
+        TimelineItem
+    } from 'vux'
     export default {
-        name: 'github',
-         components: {
-            
+        name: 'project',
+        components: {
+            Timeline,
+            TimelineItem
         },
         data() {
             return {
-                msg: 'Welcome to Your Vue.js App'
+                count: 3
             }
         }
     }
@@ -24,22 +57,18 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    h1,
-    h2 {
-        font-weight: normal;
-    }
-    
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-    
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-    
-    a {
-        color: #42b983;
-    }
+   
+        p {
+            color: #F9B0B0;
+            font-size: 0.8rem;
+        }
+        h4 {
+            color: #F9B0B0;
+            font-weight: normal;
+        }
+        strong{
+            color: #F9B0B0;
+            font-size: 0.6rem;
+        }
+ 
 </style>
--- src/components/project.vue
+++ src/components/project.vue
@@ -4,34 +4,59 @@
         <timeline-item>
             <h4>mc100压石项目网页(mobile)</h4>
             <p>2015/03–2015/07</p>
+            <p><strong>项目概要:</strong>该项目是实习期间做的一个项目，是mc100接收机配置的网页</p>
+            <p><strong>项目职责:</strong>HTML+CSS+JS的编写;页面UI的修改</p>
+            <p><strong>项目业绩:</strong>参照该项目的PC端网页,从零开始完成移动web;熟悉了HTML，CSS，Javascript;熟悉了jquery mobile，jquery等框架</p>
         </timeline-item>
         <timeline-item>
             <h4>驾考定位视频监控系统网页(pc)</h4>
             <p>2015/03–2015/07</p>
+            <p><strong>项目概要:</strong>驾考定位视频监控系统有音视频录制，播放功能;驾考定位系统包含卫星状态，车辆信息，坐标转换，音视频设置，3G设置，wifi设置 等子网页;该项目后端采用嵌入式web框架：goahead</p>
+            <p><strong>项目职责:</strong>HTML+CSS+JS的编写;页面UI的修改</p>
+            <p><strong>项目业绩:</strong>驾校系统项目网页中加入音视频播放功能，熟练掌握HTML5;熟练掌握Javascript，DOM和JSON对象，理解Ajax原理</p>
         </timeline-item>
         <timeline-item>
             <h4>N72 CORS北斗参考站接收机网页(pc,mobile)</h4>
             <p>2015/06–2015/10</p>
+            <p><strong>项目概要:</strong>N72是上海华测导航技术股份有限公司基于第三代平台研发的一款多星多频分体式北斗参考站接收机，内置蓝牙、WI-FI，精度高，稳定性强，已成功应用于各省级北斗地基增强系统的建设。 
+用户可通过网页方式对接收机进行设置。</p>
+            <p><strong>项目职责:</strong>支持IE、Google Chrome、360等浏览器访问接收机;支持多种语言。</p>
+            <p><strong>项目业绩:</strong>熟悉jquery easyui框架 ，能熟练调用框架中组件;对UI设计有了初步认识，能独立完成小页面的UI设计</p>
         </timeline-item>
         <timeline-item>
             <h4>i80北斗RTK测量仪器的自动化检测系统网页(pc,mobile)</h4>
             <p>2015/10--2017/03</p>
+            <p><strong>项目概要:</strong>项目是北斗RTK测量仪器的网页，网页旨在配置和获取仪器各模块参数，pc：jquery easyui框架，包含登录界面，主页面，60几个子页面;mobile：jquery mobile框架，包含登录界面，主页面，40几个子页面;后端：fastCGI和lighttpd</p>
+            <p><strong>项目职责:</strong>负责将UI设计制作为HTML+CSS页面;根据交互需求，完成相应Javascript编写</p>
+            <p><strong>项目业绩:</strong>精通jQuery框架，使用jQuqery,AJAX,XML熟练、准确地进行代码构建，实现所需的交互效果 ;熟悉B/S架构和常见页面布局</p>
         </timeline-item>
         <timeline-item>
             <h4>中兵集团，千寻位置，接收机定位系统(pc)</h4>
             <p>2016/01–2017/02</p>
+            <p><strong>项目概要:</strong>该项目是和中国兵器集团，千寻位置公司合作的项目</p>
+            <p><strong>项目职责:</strong>负责将UI设计制作为HTML+CSS页面;根据交互需求，完成相应Javascript编写</p>
+            <p><strong>项目业绩:</strong>有良好的审美观和用户体验意识;熟悉各浏览器的差异、原理，精通jQuery等JS框架；熟悉针对主流浏览器的代码兼容及优化</p>
         </timeline-item>
         <timeline-item>
             <h4>接收机PC网页UI改版(pc)</h4>
             <p>2016/09–2016/12</p>
+            <p><strong>项目概要:</strong>因原网页UI没改版过，领导基于网页样式提出重改pc网页样式需求。PC端网页涉及到60多个子页面，卫星跟踪，卫星图，wifi，蓝牙，网络安全，远程云服务等多个模块，原PC端网页基于jquery easyui框架，该框架是js框架，样式比较单一，为修改网页UI，我修改了easyui框架的原css文件，并在网页中引入一些jquery插件。</p>
+            <p><strong>项目职责:</strong>负责UI设计;负责将UI设计制作为HTML+CSS页面;根据交互需求，完成相应Javascript编写</p>
+            <p><strong>项目业绩:</strong>学会UI设计、切图（html、css）、交互（Javascript）这些技能;学习Bootstrap框架;充分利用了jquery 插件，echart插件等，极大地丰富了网页UI;熟练使用HTML5+CSS3模式编写WEB页面的能力</p>
         </timeline-item>
         <timeline-item>
             <h4>高精度板卡网页(pc)</h4>
             <p>2016/04–2016/08 </p>
+            <p><strong>项目概要:</strong>基于板卡的网页，通过网页可以访问板卡数据，配置板卡参数等，该项目是另一部门与本部门的合作项目，网页UI重新设计，开发类项目</p>
+            <p><strong>项目职责:</strong>负责UI设计;负责将UI设计制作为HTML+CSS页面;根据交互需求，完成相应Javascript编写</p>
+            <p><strong>项目业绩:</strong>UI框架: 考虑过amazeUI、bootstrap，最终选择bootstrap自定义样式;兼有切图（html、css）、交互（Javascript）技能;前端做好单元测试，尽量保证自己代码问题会是最少的 </p>
         </timeline-item>
         <timeline-item>
             <h4>无人船测深仪项目(pc,mobile)</h4>
             <p>2017/01–2017/02</p>
+            <p><strong>项目概要:</strong>该项目是无人船事业部成立的，网页功能在于配置测深仪参数，呈现测深仪的各类数据</p>
+            <p><strong>项目职责:</strong>负责UI设计;负责将UI设计制作为HTML+CSS页面;根据交互需求，完成相应Javascript编写</p>
+            <p><strong>项目业绩:</strong>能对前端页面自定义样式，修改jquery mobile框架自带样式，在这过程中精通CSS;配合后端人员开发运用Javascript、Ajax实现Web页面和服务器端的异步交互</p>
         </timeline-item>
     </timeline>
   </div>
@@ -58,7 +83,7 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    .timeline {
+   
         p {
             color: #888;
             font-size: 0.8rem;
@@ -67,5 +92,5 @@
             color: rgb(4, 190, 2);
             font-weight: normal;
         }
-    }
+  
 </style>
