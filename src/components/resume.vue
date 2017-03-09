c2d1c0d65b8049361762be33af2634b24f18391a
彬彬爱吃东西 <245970461@qq.com>
2017-03-09 15:11:49 +0800

    加入成果展示

--- src/components/resume.vue
+++ src/components/resume.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <div id='show' v-show="choose_show">
+      <show></show>
+    </div>
     <div id='user' v-show="choose_user">
       <user></user>
     </div>
@@ -11,14 +14,18 @@
     </div>
     <tabbar>
       <tabbar-item selected @on-item-click="ChooseUser(0)">
+        <img slot="icon" src="../assets/show.png" >
+        <span slot="label">成果展示</span>
+      </tabbar-item>
+      <tabbar-item selected @on-item-click="ChooseUser(1)">
         <img slot="icon" src="../assets/user.png" >
         <span slot="label">个人信息</span>
       </tabbar-item>
-      <tabbar-item show-dot @on-item-click="ChooseUser(1)">
+      <tabbar-item show-dot @on-item-click="ChooseUser(2)">
         <img slot="icon" src="../assets/project.png" >
         <span slot="label">项目</span>
       </tabbar-item>
-      <tabbar-item badge="2" @on-item-click="ChooseUser(2)">
+      <tabbar-item badge="2" @on-item-click="ChooseUser(3)">
         <img slot="icon" src="../assets/github.png" >
         <span slot="label">个人项目</span>
         <div slot="default">
@@ -40,9 +47,10 @@
     import User from 'components/user'
     import Project from 'components/project'
     import Github from 'components/github'
-
+    import Show from 'components/show'
     export default {
         components: {
+            Show,
             User,
             Project,
             Github,
@@ -53,9 +61,10 @@
         },
         data() {
             return {
-                choose_user: true,
+                choose_user: false,
                 choose_project: false,
-                choose_github: false
+                choose_github: false,
+                choose_show: true
             }
         },
         methods: {
@@ -64,12 +73,15 @@
                 this.choose_user = false
                 this.choose_project = false
                 this.choose_github = false
+                this.choose_show = false
 
                 if (idx == 0)
-                    this.choose_user = true
+                    this.choose_show = true
                 else if (idx == 1)
-                    this.choose_project = true
+                    this.choose_user = true
                 else if (idx == 2)
+                    this.choose_project = true
+                else if (idx == 3)
                     this.choose_github = true
             }
         }
---
+++ src/components/show.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="show">
+   <divider>pc page</divider>
+   <swiper auto height="300px">
+      <swiper-item class="black"> <img src="../assets/i80NewImage/1.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80NewImage/2.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80NewImage/3.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80NewImage/4.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80NewImage/5.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80NewImage/6.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80NewImage/7.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80NewImage/8.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80NewImage/9.png" class="imgSize"></img></swiper-item>
+    </swiper>
+    <divider>mobile page</divider>
+    <swiper auto height="500px">
+      <swiper-item class="black"> <img src="../assets/i80MobileImage/1.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80MobileImage/2.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80MobileImage/3.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80MobileImage/4.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80MobileImage/5.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80MobileImage/6.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80MobileImage/7.png" class="imgSize"></img></swiper-item>
+    </swiper>
+    <divider>pc page</divider>
+    <swiper auto height="300px">
+      <swiper-item class="black"> <img src="../assets/i80MobileImage/1.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80MobileImage/2.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80MobileImage/3.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80MobileImage/4.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80MobileImage/5.png" class="imgSize"></img></swiper-item>
+      <swiper-item class="black"> <img src="../assets/i80MobileImage/6.png" class="imgSize"></img></swiper-item>
+      
+    </swiper>
+
+</template>
+
+<script>
+    import {
+       Swiper,
+    SwiperItem,
+    GroupTitle,
+    XButton,
+    Divider
+    } from 'vux'
+    //import {Echarts} from '../assets/lib/echarts.min.js'
+   
+    //import MyCell from "components/mycell"
+    export default {
+        name: 'show',
+        components: {
+            Swiper,
+    SwiperItem,
+    GroupTitle,
+    XButton,
+    Divider
+        },
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        },
+   
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+   .imgSize{
+   width:100%;
+   height:100%;
+   }
+</style>
