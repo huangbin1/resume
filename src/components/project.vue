e01fb5013adaaaadc4f4fd9f42b073abd593b71b
huangbin1 <245970461@qq.com>
2017-03-11 21:56:08 +0800

    更改项目样式

--- src/components/project.vue
+++ src/components/project.vue
@@ -3,60 +3,76 @@
     <timeline class="timeline">
         <timeline-item>
             <h4>MC100定位定向仪器（mobile）</h4>
-            <p>2015/03–2015/07</p>
-            <p><strong>项目概要:</strong>MC100定位定向仪器由一个主机、两个GPS天线和以嵌入式主板为核心组成，采用先进的一机双天线、载波相位差分技术，实时计算出两个天线的位置和方位角。</p>
-            <p><strong>项目职责:</strong>HTML+CSS+JS+UI DESIGN</p>
-            <p><strong>项目业绩:</strong>参照该项目的PC端网页,从零开始完成移动web;熟悉了HTML，CSS，Javascript;熟悉了jquery mobile，jquery等框架</p>
+            <div class="item_des">
+                <p>2015/03–2015/07</p>
+                <p><strong>项目概要:</strong>MC100定位定向仪器由一个主机、两个GPS天线和以嵌入式主板为核心组成，采用先进的一机双天线、载波相位差分技术，实时计算出两个天线的位置和方位角。</p>
+                <p><strong>项目职责:</strong>HTML+CSS+JS+UI DESIGN</p>
+                <p><strong>项目业绩:</strong>参照该项目的PC端网页,从零开始完成移动web;熟悉了HTML，CSS，Javascript;熟悉了jquery mobile，jquery等框架</p>
+            </div>
         </timeline-item>
         <timeline-item>
             <h4>卫星定位车辆监控系统（pc）</h4>
-            <p>2015/03–2015/07</p>
-            <p><strong>项目概要:</strong>卫星定位车辆监控系统是利用数据采集、移动通信与互联网技术，把车辆的位置、状态等数据反馈给车辆管理人员软件；包含卫星状态，车辆信息，坐标转换，音视频设置，3G设置，wifi设置 等子网页；该项目后端采用嵌入式web框架：goahead</p>
-            <p><strong>项目职责:</strong>HTML+CSS+JS+UI DESIGN</p>
-            <p><strong>项目业绩:</strong>掌握HTML5，JS，DOM，JSON，Ajax</p>
+            <div class="item_des">
+                <p>2015/03–2015/07</p>
+                <p><strong>项目概要:</strong>卫星定位车辆监控系统是利用数据采集、移动通信与互联网技术，把车辆的位置、状态等数据反馈给车辆管理人员软件；包含卫星状态，车辆信息，坐标转换，音视频设置，3G设置，wifi设置 等子网页；该项目后端采用嵌入式web框架：goahead</p>
+                <p><strong>项目职责:</strong>HTML+CSS+JS+UI DESIGN</p>
+                <p><strong>项目业绩:</strong>掌握HTML5，JS，DOM，JSON，Ajax</p>
+            </div>
         </timeline-item>
         <timeline-item>
             <h4>N72 CORS北斗参考站接收机网页(pc,mobile)</h4>
-            <p>2015/06–2015/10</p>
-            <p><strong>项目概要:</strong>N72是上海华测导航技术股份有限公司基于第三代平台研发的一款多星多频分体式北斗参考站接收机，内置蓝牙、WI-FI，精度高，稳定性强，已成功应用于各省级北斗地基增强系统的建设。 
-用户可通过网页方式对接收机进行设置。</p>
-            <p><strong>项目职责:</strong>支持IE、Google Chrome、360等浏览器访问接收机;支持多种语言。</p>
-            <p><strong>项目业绩:</strong>熟悉jquery easyui框架 ，能熟练调用框架中组件;对UI设计有了初步认识，能独立完成小页面的UI设计</p>
+            <div class="item_des">
+                <p>2015/06–2015/10</p>
+                <p><strong>项目概要:</strong>N72是上海华测导航技术股份有限公司基于第三代平台研发的一款多星多频分体式北斗参考站接收机，内置蓝牙、WI-FI，精度高，稳定性强，已成功应用于各省级北斗地基增强系统的建设。 
+    用户可通过网页方式对接收机进行设置。</p>
+                <p><strong>项目职责:</strong>支持IE、Google Chrome、360等浏览器访问接收机;支持多种语言。</p>
+                <p><strong>项目业绩:</strong>熟悉jquery easyui框架 ，能熟练调用框架中组件;对UI设计有了初步认识，能独立完成小页面的UI设计</p>
+            </div>
         </timeline-item>
         <timeline-item>
             <h4>i80北斗RTK测量系统(pc,mobile)</h4>
-            <p>2015/10--2017/03</p>
-            <p><strong>项目概要:</strong>项目是北斗RTK测量仪器的网页，网页旨在配置和获取仪器各模块参数，pc：jquery easyui框架，包含登录界面，主页面，60几个子页面;mobile：jquery mobile框架，包含登录界面，主页面，40几个子页面;后端：fastCGI和lighttpd</p>
-            <p><strong>项目职责:</strong>负责将UI设计制作为HTML+CSS页面;根据交互需求，完成相应Javascript编写</p>
-            <p><strong>项目业绩:</strong>精通jQuery框架，使用jQuqery,AJAX,XML熟练、准确地进行代码构建，实现所需的交互效果 ;熟悉B/S架构和常见页面布局</p>
+            <div class="item_des">
+                <p>2015/10--2017/03</p>
+                <p><strong>项目概要:</strong>项目是北斗RTK测量仪器的网页，网页旨在配置和获取仪器各模块参数，pc：jquery easyui框架，包含登录界面，主页面，60几个子页面;mobile：jquery mobile框架，包含登录界面，主页面，40几个子页面;后端：fastCGI和lighttpd</p>
+                <p><strong>项目职责:</strong>负责将UI设计制作为HTML+CSS页面;根据交互需求，完成相应Javascript编写</p>
+                <p><strong>项目业绩:</strong>精通jQuery框架，使用jQuqery,AJAX,XML熟练、准确地进行代码构建，实现所需的交互效果 ;熟悉B/S架构和常见页面布局</p>
+            </div>
         </timeline-item>
         <timeline-item>
             <h4>气象仪（pc）</h4>
-            <p>2016/01–2017/02</p>
-            <p><strong>项目概要:</strong>气象仪用来获取风，湿度，温度，压力，降雨量和环境参数。</p>
-            <p><strong>项目职责:</strong>负责将UI设计制作为HTML+CSS页面;根据交互需求，完成相应Javascript编写</p>
-            <p><strong>项目业绩:</strong>有良好的审美观和用户体验意识;熟悉各浏览器的差异、原理，精通jQuery等JS框架；熟悉针对主流浏览器的代码兼容及优化</p>
+            <div class="item_des">
+                <p>2016/01–2017/02</p>
+                <p><strong>项目概要:</strong>气象仪用来获取风，湿度，温度，压力，降雨量和环境参数。</p>
+                <p><strong>项目职责:</strong>负责将UI设计制作为HTML+CSS页面;根据交互需求，完成相应Javascript编写</p>
+                <p><strong>项目业绩:</strong>有良好的审美观和用户体验意识;熟悉各浏览器的差异、原理，精通jQuery等JS框架；熟悉针对主流浏览器的代码兼容及优化</p>
+            </div>
         </timeline-item>
         <timeline-item>
             <h4>接收机PC网页UI改版(pc)</h4>
-            <p>2016/09–2016/12</p>
-            <p><strong>项目概要:</strong>因原网页UI没改版过，领导基于网页样式提出重改pc网页样式需求。PC端网页涉及到60多个子页面，卫星跟踪，卫星图，wifi，蓝牙，网络安全，远程云服务等多个模块，原PC端网页基于jquery easyui框架，该框架是js框架，样式比较单一，为修改网页UI，我修改了easyui框架的原css文件，并在网页中引入一些jquery插件。</p>
-            <p><strong>项目职责:</strong>负责UI设计;负责将UI设计制作为HTML+CSS页面;根据交互需求，完成相应Javascript编写</p>
-            <p><strong>项目业绩:</strong>学会UI设计、切图（html、css）、交互（Javascript）这些技能;学习Bootstrap框架;充分利用了jquery 插件，echart插件等，极大地丰富了网页UI;熟练使用HTML5+CSS3模式编写WEB页面的能力</p>
+            <div class="item_des">
+                <p>2016/09–2016/12</p>
+                <p><strong>项目概要:</strong>因原网页UI没改版过，领导基于网页样式提出重改pc网页样式需求。PC端网页涉及到60多个子页面，卫星跟踪，卫星图，wifi，蓝牙，网络安全，远程云服务等多个模块，原PC端网页基于jquery easyui框架，该框架是js框架，样式比较单一，为修改网页UI，我修改了easyui框架的原css文件，并在网页中引入一些jquery插件。</p>
+                <p><strong>项目职责:</strong>负责UI设计;负责将UI设计制作为HTML+CSS页面;根据交互需求，完成相应Javascript编写</p>
+                <p><strong>项目业绩:</strong>学会UI设计、切图（html、css）、交互（Javascript）这些技能;学习Bootstrap框架;充分利用了jquery 插件，echart插件等，极大地丰富了网页UI;熟练使用HTML5+CSS3模式编写WEB页面的能力</p>
+            </div>
         </timeline-item>
         <timeline-item>
             <h4>高精度板卡网页(pc)</h4>
-            <p>2016/04–2016/08 </p>
-            <p><strong>项目概要:</strong>基于高精度高动态定位定向OEM主板，通过网页可以访问板卡数据，配置板卡参数等</p>
-            <p><strong>项目职责:</strong>负责HTML+CSS+JS+UI DESIGN</p>
-            <p><strong>项目业绩:</strong>UI框架: 考虑过amazeUI、bootstrap，最终选择bootstrap自定义样式;兼有切图（html、css）、交互（Javascript）技能;前端做好单元测试，尽量保证自己代码问题会是最少的 </p>
+            <div class="item_des">
+                <p>2016/04–2016/08 </p>
+                <p><strong>项目概要:</strong>基于高精度高动态定位定向OEM主板，通过网页可以访问板卡数据，配置板卡参数等</p>
+                <p><strong>项目职责:</strong>负责HTML+CSS+JS+UI DESIGN</p>
+                <p><strong>项目业绩:</strong>UI框架: 考虑过amazeUI、bootstrap，最终选择bootstrap自定义样式;兼有切图（html、css）、交互（Javascript）技能;前端做好单元测试，尽量保证自己代码问题会是最少的 </p>
+            </div>
         </timeline-item>
         <timeline-item>
             <h4>无人船测深仪项目(pc,mobile)</h4>
-            <p>2017/01–2017/02</p>
-            <p><strong>项目概要:</strong>测声仪用来测量 吃水深度，温度，AD数值等</p>
-            <p><strong>项目职责:</strong>负责HTML+CSS+JS+UI DESIGN</p>
-            <p><strong>项目业绩:</strong>能对前端页面自定义样式，修改jquery mobile框架自带样式，在这过程中精通CSS;配合后端人员开发运用Javascript、Ajax实现Web页面和服务器端的异步交互</p>
+            <div class="item_des">
+                <p>2017/01–2017/02</p>
+                <p><strong>项目概要:</strong>测声仪用来测量 吃水深度，温度，AD数值等</p>
+                <p><strong>项目职责:</strong>负责HTML+CSS+JS+UI DESIGN</p>
+                <p><strong>项目业绩:</strong>能对前端页面自定义样式，修改jquery mobile框架自带样式，在这过程中精通CSS;配合后端人员开发运用Javascript、Ajax实现Web页面和服务器端的异步交互</p>
+            </div>
         </timeline-item>
     </timeline>
   </div>
@@ -100,8 +116,15 @@
             color: #66D3EE;
         }
         .project{
-        background: linear-gradient(to bottom right, #99EBFF, white);
         padding-bottom:3.2rem;
         }
       
+      .item_des
+        {
+        border: 2px dashed #FEDEFE;
+        border-radius: 10px;
+        margin-top: 20px;
+        background-color: #FEDCD0;
+        padding:10px;
+        }
 </style>
